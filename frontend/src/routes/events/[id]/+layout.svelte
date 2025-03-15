b939b425ca6a0f9aff9fbf4cb4cab6664f24ad7a
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-03-14 22:40:05 -0500

    fix: change event description text color to accent
    Previously, it was a grey that didn't look good on all themes

--- frontend/src/routes/events/[id]/+layout.svelte
+++ frontend/src/routes/events/[id]/+layout.svelte
@@ -4,7 +4,7 @@
 
 <div class="text-center p-4">
     <h1 class="text-2xl font-semibold">{data.event.name}</h1>
-    <p class="text-gray-600">{data.event.description}</p>
+    <p class="text-accent">{data.event.description}</p>
   </div>
 
 {@render children()}
