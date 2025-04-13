fe0499e3a184d31bcd3192a4aca7a6cbff110540
Vahin Mehra <vahin@vahin.dev>
2025-04-13 19:06:44 +0100

    Removed email from UserPrivate and made it inherit from UserSignupPayload

--- backend/podium/db/user.py
+++ backend/podium/db/user.py
@@ -58,7 +58,7 @@ class UserSignupPayload(UserBase):
 
 
 
-class UserPrivate(UserBase):
+class UserPrivate(UserSignupPayload):
     id: Annotated[str, StringConstraints(pattern=constants.RECORD_REGEX)]
     votes: constants.MultiRecordField = []
     projects: constants.MultiRecordField = []
@@ -66,7 +66,6 @@ class UserPrivate(UserBase):
     owned_events: constants.MultiRecordField = []
     attending_events: constants.MultiRecordField = []
     referral: constants.MultiRecordField = []
-    email: EmailStrippedLower
 
 
 
