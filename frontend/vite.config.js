c83c4804bf0d656ce9ada53ec1e35d908eede274
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-06-12 20:13:39 +0200

    chore: update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-// import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
