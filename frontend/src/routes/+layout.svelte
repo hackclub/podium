f2c6daac9ae5edee7061e1bc10aba686b8ddc633
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-06-23 22:42:30 +0300

    feat: center logo

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -69,37 +69,44 @@
   <div class="drawer-content flex flex-col">
     <!-- Navbar -->
     <div class="navbar bg-base-300 w-full">
-      <div class="flex-none lg:hidden">
-        <label
-          for="my-drawer-3"
-          aria-label="open sidebar"
-          class="btn btn-square btn-ghost"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block h-6 w-6 stroke-current"
+      <div class="navbar-start">
+        <div class="lg:hidden">
+          <label
+            for="my-drawer-3"
+            aria-label="open sidebar"
+            class="btn btn-square btn-ghost"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </label>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block h-6 w-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+        </div>
       </div>
       
-      <div class="mx-2 flex-1 px-2"><a href="/" class="font-extrabold">Podium</a></div>
-      <div class="hidden flex-none lg:block">
-        <ul class="menu menu-horizontal">
-          {#each Object.entries(navOptions) as [path, label]}
-            <li>
-              <a href={path}>{label}</a>
-            </li>
-          {/each}
-        </ul>
+      <div class="navbar-center">
+        <a href="/" class="btn btn-ghost text-xl font-extrabold">Podium</a>
+      </div>
+      
+      <div class="navbar-end">
+        <div class="hidden lg:block">
+          <ul class="menu menu-horizontal px-1">
+            {#each Object.entries(navOptions) as [path, label]}
+              <li>
+                <a href={path}>{label}</a>
+              </li>
+            {/each}
+          </ul>
+        </div>
       </div>
     </div>
     <!--------- start of page content --------->
