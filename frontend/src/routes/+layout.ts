09ce8bc4e1ceb33ae98c89fff1c78b26d671fad1
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-09-28 01:09:33 -0500

    chore: add back redirect

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,8 +1,11 @@
+import { redirect } from "@sveltejs/kit";
+
 // Until fetch is passed to the API client, this should prevent from the client being authenticated but an unauthenticated request being made from the server
 export const ssr = false;
 
 // Provide default meta tags for all pages
 export const load = () => {
+  redirect(307, "https://hack.club/submit");
   return {
     meta: [
       {
