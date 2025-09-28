101641defbbed9881ef1dd4d6d796257d066af34
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-09-27 22:43:40 -0500

    feat: remove redirect

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,9 +1,9 @@
-import { redirect } from "@sveltejs/kit";
+// import { redirect } from "@sveltejs/kit";
 
-export function load() {
-	if (import.meta.env.DEV) { 
-		console.log("In dev, not redirecting to hack.club/submit");
-		return;
-	}
-	redirect(307, "https://hack.club/submit");
-}
+// export function load() {
+// 	if (import.meta.env.DEV) { 
+// 		console.log("In dev, not redirecting to hack.club/submit");
+// 		return;
+// 	}
+// 	redirect(307, "https://hack.club/submit");
+// }
