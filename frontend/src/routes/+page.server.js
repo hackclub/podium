418200deca898c63d90dab6cfecc7c4794b23d8e
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-09-27 23:35:54 -0500

    feat: disable redirect to fillout

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,9 +1,9 @@
-import { redirect } from "@sveltejs/kit";
-import { REDIRECT_TO_FILLOUT_FORM } from "$env/dynamic/public";
-export function load() {
-	if (REDIRECT_TO_FILLOUT_FORM !== "true") { 
-		console.log("Not redirecting to hack.club/submit");
-		return;
-	}
-	redirect(307, "https://hack.club/submit");
-}
+// import { redirect } from "@sveltejs/kit";
+// import { REDIRECT_TO_FILLOUT_FORM } from "$env/dynamic/public";
+// export function load() {
+// 	if (REDIRECT_TO_FILLOUT_FORM !== "true") { 
+// 		console.log("Not redirecting to hack.club/submit");
+// 		return;
+// 	}
+// 	redirect(307, "https://hack.club/submit");
+// }
